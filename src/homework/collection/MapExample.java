07c266665e160d86db988b250b4baaf99669997a
SimonyanPash777 <simonyan.2004.pash@mail.ru>
2022-08-05 01:25:54 +0400

    Collection

--- src/homework/collection/MapExample.java
+++ src/homework/collection/MapExample.java
@@ -6,22 +6,32 @@ public class MapExample {
 
     public static void main(String[] args) {
         List<FootballTeamMember> teamPlayers = new ArrayList<>();
-        teamPlayers.add(new FootballTeamMember(1,"Poxos"));
-        teamPlayers.add(new FootballTeamMember(2,"Mardiros"));
-        teamPlayers.add(new FootballTeamMember(3,"Petros"));
-        teamPlayers.add(new FootballTeamMember(4,"Buffon"));
-        Map<Integer, String> teamPlayersName = new HashMap<>();
-        teamPlayersName.put(1,"Aram Sargsyan");
-        teamPlayersName.put(2,"Zaven Voskerchyan");
+        teamPlayers.add(new FootballTeamMember(1, "Poxos"));
+        teamPlayers.add(new FootballTeamMember(2, "Mardiros"));
+        teamPlayers.add(new FootballTeamMember(3, "Petros"));
+        teamPlayers.add(new FootballTeamMember(4, "Buffon"));
         System.out.println(createFootballTeam(teamPlayers));
-        System.out.println(removeFromMap("Petros", 3));
+        Map<Integer, String> teamPlayersName = new HashMap<>();
+        teamPlayersName.put(1, "Aram Sargsyan");
+        teamPlayersName.put(2, "Zaven Voskerchyan");
+        teamPlayersName.put(3, "poxos poxosyan");
+        teamPlayersName.put(4, "petros petrosyan");
+        teamPlayersName.put(5, "martiros martirosyan");
+        System.out.println();
         printAllMemberNames(teamPlayersName);
+        System.out.println();
+        printAllMembers(teamPlayersName);
+        System.out.println(removeFromMap(3, "poxos poxosyan"));
+        System.out.println(teamPlayersName);
+        System.out.println();
+        printAllMemberNames(teamPlayersName);
+        System.out.println();
         printAllMembers(teamPlayersName);
     }
 
     //Ունենք FootballTeamMember-ի լիստ, պետք է ստանանք HashMap որտեղ կեյ-ը կլինի խաղացողի համարը, իսկ վելյուն իրա անունը։
     static Map<Integer, String> createFootballTeam(List<FootballTeamMember> members) {
-        Map<Integer, String> crateTeam  = new HashMap<>();
+        Map<Integer, String> crateTeam = new HashMap<>();
         for (FootballTeamMember member : members) {
             crateTeam.put(member.getNumber(), member.getName());
             if (crateTeam.size() == members.size()) return crateTeam;
@@ -30,14 +40,17 @@ public class MapExample {
     }
 
     //Մեթոդի մեջ պետք է տրված մապ-ից ջնջենք removedNumber համարը եթե կա, ու վերադարձնենք true, եթե չկա վերադարձնենք false
-    static boolean removeFromMap(String memberMap, Integer removedNumber) {
-        Map<Integer, String> states = new HashMap<>();
-        for (Integer integer : states.keySet()) {
-            if (integer != removedNumber) {
-                return true;
-            }
+    static boolean removeFromMap(Integer key, String value) {
+        HashMap<Integer, String> result;
+        result = new HashMap<>();
+        if (result.get(key) == null) {
+            return true;
+        } else {
+            result.remove(key, value);
+            return false;
         }
-        return false;
+
+
     }
 
     //Մեթոդով տպելու ենք միայն անունները
@@ -51,8 +64,8 @@ public class MapExample {
     // 11 - Poxos Poxosyan
     // 12 - Petros Petrosyan
     static void printAllMembers(Map<Integer, String> memberMap) {
-        for( Map.Entry<Integer, String> entry : memberMap.entrySet() ){
-            System.out.println( entry.getKey() + " - " + entry.getValue() );
+        for (Map.Entry<Integer, String> entry : memberMap.entrySet()) {
+            System.out.println(entry.getKey() + " - " + entry.getValue());
         }
     }
 
